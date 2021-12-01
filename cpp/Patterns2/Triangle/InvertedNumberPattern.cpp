c2d9b569c39d5c02313dba06ce0b7af7220854a6
RKSINHA <sinharohit3333@gmail.com>
2021-12-01 19:36:22 +0530

    add: Inverted number pattern code

---
+++ cpp/Patterns2/Triangle/InvertedNumberPattern.cpp
@@ -0,0 +1,31 @@
+/* Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+333
+22
+1 */
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n){
+    int j = 1;
+    while (j <= n - i + 1){
+     cout << n - i + 1;
+     j = j + 1;
+    }
+    cout << endl;
+    i = i + 1;
+    }
+
+    return 0;
+  
+}
+
+
