d769b21a049d1dd05f85ad202ea61c6e2d6ea39f
RKSINHA <sinharohit3333@gmail.com>
2021-12-01 15:17:11 +0530

    add: squarePattern code

---
+++ cpp/Patterns1/squarePattern.cpp
@@ -0,0 +1,32 @@
+/* Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444 
+4444 */
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n){
+    int j = 1;
+    while (j <= n){
+     cout << n;
+     j = j + 1;
+    }
+    cout << endl;
+    i = i + 1;
+    }
+
+    return 0;
+  
+}
+
+
