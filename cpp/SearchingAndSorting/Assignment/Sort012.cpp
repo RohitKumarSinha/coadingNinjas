e175c64b76f2563e92a02a8b54bc8ce18b86c37c
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-14 23:22:54 +0530

    modify code sort 0 1 2

--- cpp/SearchingAndSorting/Assignment/Sort012.cpp
+++ cpp/SearchingAndSorting/Assignment/Sort012.cpp
@@ -1,6 +1,6 @@
-/* You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s. Write a solution to sort this 
+/* You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s. Write a solution to sort this
 array/list in a 'single scan'.
-'Single Scan' refers to iterating over the array/list just once or to put it in other words, 
+'Single Scan' refers to iterating over the array/list just once or to put it in other words,
 you will be visiting each element in the array/list just once.
 
 Sample Input 1:
@@ -10,30 +10,44 @@ Sample Input 1:
 Sample Output 1:
 0 0 0 1 1 2 2 */
 
-
-
-
 void sort012(int *arr, int n)
 {
-    //Write your code here
-    int count0 = 0,count1 = 0, count2 = 0;
-    for(int i = 0; i < n; i++){    //count 0   
-        if(arr[i] == 0){
-            count0++;
+    // check whether the size of the array is less than one or not
+    if (n <= 1)
+    {
+        return;
+    }
+    else
+    {
+        // making count variable to keep track of total number of zero and one in array
+        int count_zero = 0;
+        int count_one = 0;
+        // updating those variable using loop
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == 0)
+            {
+                count_zero++;
+            }
+            if (arr[i] == 1)
+            {
+                count_one++;
+            }
         }
-        if(arr[i] == 1){    //count 1
-            count1++;
-        }  
-    }
-    
-    for(int i=0;i<count0;i++){    //print 0
-        arr[i]=0;
-    }
-    int temp = count0 + count1;
-     for(int i=count0;i<temp;i++){    //print 1
-        arr[i]=1;
-    }
-     for(int i=temp;i<n;i++){    //print 2
-        arr[i]=2;
+        // reinitializing the variable firstly zero then one then two using loop
+        int i = 0;
+        for (i; i < count_zero; i++)
+        {
+            arr[i] = 0;
+        }
+        for (int j = 0; j < count_one; j++)
+        {
+            arr[i] = 1;
+            i++;
+        }
+        for (i; i < n; i++)
+        {
+            arr[i] = 2;
+        }
     }
 }
