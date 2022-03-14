46dcb3f08c922bc7125efc21c7265b9ce49665c5
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-14 22:49:51 +0530

    modify: SecondLargestInArray code

--- cpp/SearchingAndSorting/Assignment/SecondLargestInArray.cpp
+++ cpp/SearchingAndSorting/Assignment/SecondLargestInArray.cpp
@@ -1,4 +1,4 @@
-/* You have been given a random integer array/list(ARR) of size N. You are required to find and return the second largest 
+/* You have been given a random integer array/list(ARR) of size N. You are required to find and return the second largest
 element present in the array/list.
 If N <= 1 or all the elements are same in the array/list then return -2147483648 or -2 ^ 31
 (It is the smallest value for the range of Integer)
@@ -9,34 +9,36 @@ Sample Input 1:
 2 13 4 1 3 6 28
 Sample Output 1:
 13 */
-
+#include <climits>
 int findSecondLargest(int *input, int n)
 {
-    //Write your code here
-    long a = 2147483648;
-     int count=0;int i=0,j=1;
-    for(j;j<n;j++){
-        if(input[i] == input[j]){
-        	count++;
-        }
-    }
-    if((count)==n-1 || n <= 1)
-    	return -a;
-    else{
-        int max = -9999999;
-	    int j = 0;
-	    for(int i = 0; i < n; i++){
-		    if(input[i] > max){
-			    max = input[i];
-		    }
-	    }
-	    int temp = max;
-	    max = -9999999;
-	    for(int i = 0; i < n; i++){
-		    if(input[i] > max && input[i] != temp){
-			    max = input[i];
-		    }
-	    }
-        return max;
+	// check whether the size of the array is less than one or not
+	if (n <= 1)
+	{
+		return INT_MIN;
+	}
+
+	else
+	{
+		// find the maximum of the array
+		int maxi = input[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (input[i] > maxi)
+			{
+				maxi = input[i];
+			}
+		}
+		// with the help of maximum find the second maximum of the array
+		int sec_max = INT_MIN;
+		for (int i = 0; i < n; i++)
+		{
+			if (input[i] < maxi && input[i] > sec_max)
+			{
+				sec_max = input[i];
+			}
+		}
+
+		return sec_max;
 	}
 }
