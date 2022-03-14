5b078eae6e73f92624decccff23536d0cdcaa34a
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-14 21:46:00 +0530

    modify: Rotate Array Code

--- cpp/SearchingAndSorting/Assignment/RotateArray.cpp
+++ cpp/SearchingAndSorting/Assignment/RotateArray.cpp
@@ -1,4 +1,4 @@
-/* You have been given a random integer array/list(ARR) of size N. Write a function that rotates the given array/list by 
+/* You have been given a random integer array/list(ARR) of size N. Write a function that rotates the given array/list by
 D elements(towards the left).
 
 Sample Input 1:
@@ -11,22 +11,25 @@ Sample Output 1:
 
 void rotate(int *input, int d, int n)
 {
-    //Write your code here
-    
+    // copying the starting d element in new array
     int arr[d];
-    for(int i = 0; i < d; i++){
+    for (int i = 0; i < d; i++)
+    {
         arr[i] = input[i];
     }
-    
-    int j;
-    for(j = 0; j < n-d; j++){
-        input[j] = input[j + d];
+    // shifting the index of input array
+    int j = 0;
+    for (int i = d; i < n; i++)
+    {
+        input[j] = input[i];
+        j++;
     }
+    // pasting the new array value to the last of the input array
     int k = 0;
-    for(j; j < n; j++){
+    while (j < n)
+    {
         input[j] = arr[k];
         k++;
+        j++;
     }
-    
-    
 }
