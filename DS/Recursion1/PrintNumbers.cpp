75fa75860c7d2c680e3347b7ffcdaa37fccc383f
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-18 20:19:28 +0530

    modify: print 1 to n recursion

--- DS/Recursion1/PrintNumbers.cpp
+++ DS/Recursion1/PrintNumbers.cpp
@@ -1,18 +1,20 @@
-/* Given is the code to print numbers from 1 to n in increasing order recursively. 
+/* Given is the code to print numbers from 1 to n in increasing order recursively.
 But it contains few bugs that you need to rectify such that all the test cases pass.
 
 Sample Input 1 :
  6
 Sample Output 1 :
 1 2 3 4 5 6 */
-
-int N = 1;
-void print(int n){
-    if(n == 1){
-        cout << N << " ";
+#include <iostream>
+using namespace std;
+void print(int n)
+{
+    if (n <= 0)
+    {
         return;
     }
-    cout << N << " ";
-    N++;
+
     print(n - 1);
+    // when the base case returns then we print the n in ascending order from 1
+    cout << n << " ";
 }
