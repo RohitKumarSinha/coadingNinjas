dbc842740e5272dfb5882f543809304a5c4ea79d
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-22 22:14:59 +0530

    modify checkPalindrome recursion

--- DS/Recursion1/Assignment/checkPalindrome.cpp
+++ DS/Recursion1/Assignment/checkPalindrome.cpp
@@ -8,28 +8,33 @@ ninja
 Sample Output 2:
 false */
 
-int length(char input[]) { 
-    int len = 0;
-    for(int i = 0; input[i] != '\0'; i++) { 
-        len++; 
-    }
-    return len;
-} 
-bool checkPalindrome(char input[], int start, int end) {
-    if(input[0] == '\0' || input[1] == '\0') {
+#include <cstring>
+
+bool checkPalindrome(char input[], int start, int end)
+{
+    if (input[0] == '\0' || input[1] == '\0')
+    {
         return true;
     }
-    if(start >= end) {
+
+    if (start >= end)
+    {
         return true;
     }
-    if(input[start] == input[end]) {
-        return checkPalindrome(input, start + 1, end - 1); 
-    } 
-    else { 
+
+    if (input[start] != input[end])
+    {
         return false;
     }
+
+    if (input[start] == input[end])
+    {
+        return checkPalindrome(input, start + 1, end - 1);
+    }
 }
-bool checkPalindrome(char input[]) { 
-    int len = length(input);
-    return checkPalindrome(input, 0, len - 1);
+
+bool checkPalindrome(char input[])
+{
+    int length = strlen(input);
+    return checkPalindrome(input, 0, length - 1);
 }
