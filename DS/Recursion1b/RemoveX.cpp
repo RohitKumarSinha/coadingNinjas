91ed8e3dd796cb13595239a933b25f17e3345e5c
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-23 14:51:16 +0530

    modify: Remove X program recursion

--- DS/Recursion1b/RemoveX.cpp
+++ DS/Recursion1b/RemoveX.cpp
@@ -4,23 +4,27 @@ xaxb
 Sample Output 1:
 ab
 Sample Input 2 :
-abc 
+abc
 Sample Output 2:
 abc */
 
 // Change in the given string itself. So no need to return or print anything
 
-void removeX(char input[]) {
+void removeX(char input[])
+{
     // Write your code here
-    if (input[0] == '\0'){
+    if (input[0] == '\0')
+    {
         return;
     }
 
-    removeX(input+1);
+    removeX(input + 1);
 
-    if (input[0] == 'x'){
-        for(int i = 0; input[i] != '\0'; i++){
-            input[i] = input[i+1];
+    if (input[0] == 'x' || input[0] == 'X')
+    {
+        for (int i = 0; input[i] != '\0'; i++)
+        {
+            input[i] = input[i + 1];
         }
     }
 }
