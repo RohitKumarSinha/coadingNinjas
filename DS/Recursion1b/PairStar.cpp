3ea1d1f6f69dd55e80db90a222e236a253e4fcf0
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-23 17:53:09 +0530

    modify pair Star code in recursion

--- DS/Recursion1b/PairStar.cpp
+++ DS/Recursion1b/PairStar.cpp
@@ -1,4 +1,4 @@
-/* Given a string S, compute recursively a new string where identical chars that are adjacent in the original 
+/* Given a string S, compute recursively a new string where identical chars that are adjacent in the original
 string are separated from each other by a "*".
 Sample Input 1 :
 hello
@@ -10,20 +10,24 @@ Sample Output 2 :
 a*a*a*a */
 
 // Change in the given string itself. So no need to return or print the changed string.
-#include<bits/stdc++.h>
-void pairStar(char input[]) {
-    // Write your code here
-    // Write your code here
-    if(strlen(input) == 0) return;
+#include <cstring>
+void pairStar(char input[])
+{
+    if (input[0] == '\0')
+    {
+        return;
+    }
 
-    if (input[0] == input[1]){
-         for(int i=strlen(input); i>=1; i--){
-            input[i+1] = input[i];
+    if (input[0] == input[1])
+    {
+        // we are looping in back side because we can easily shift the value using loop back side
+        for (int i = strlen(input); i >= 1; i--)
+        {
+            input[i + 1] = input[i];
         }
         input[1] = '*';
         pairStar(input + 2);
     }
-    else {
-        pairStar(input+1);
-    }
+
+    pairStar(input + 1);
 }
