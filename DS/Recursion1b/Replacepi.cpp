ce9fc14cf48d0e59d8fa91a495219046c33b1e33
ROHITKRSINHA <sinharohit3333@gmail.com>
2022-03-23 18:02:24 +0530

    modify: Replace pi code recursion

--- DS/Recursion1b/Replacepi.cpp
+++ DS/Recursion1b/Replacepi.cpp
@@ -4,28 +4,36 @@ xpix
 Sample Output :
 x3.14x
 Sample Input 2 :
-pipi 
+pipi
 Sample Output :
 3.143.14
 */
 
 // Change in the given string itself. So no need to return or print anything
-#include<bits/stdc++.h>
-void replacePi(char input[]) {
-	// Write your code here
-     if(strlen(input) <= 1) 
+#include <cstring>
+void replacePi(char input[])
+{
+    if (input[0] == '\0')
+    {
         return;
-    if(input[0] == 'p' && input[1] == 'i'){
-        for(int i=strlen(input); i>=2; i--) 
-            input[i+2] = input[i];
-        
+    }
+
+    if (input[0] == 'p' && input[1] == 'i')
+    {
+        // looping backside for shifting the value 2 because we need 2 extra space of putting the value 1, 4
+        // and 3 and . goes to the position p i
+        for (int i = strlen(input); i >= 2; i--)
+        {
+            input[i + 2] = input[i];
+        }
         input[0] = '3';
         input[1] = '.';
         input[2] = '1';
         input[3] = '4';
-        return replacePi(input + 4);
-    } 
-    else return replacePi(input + 1);
+        replacePi(input + 4);
+    }
+    else
+    {
+        replacePi(input + 1);
+    }
 }
-
-
